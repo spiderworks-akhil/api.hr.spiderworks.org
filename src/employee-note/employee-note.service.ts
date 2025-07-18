ba326c0ff614a6b77f2254eda97258d324942a03
Roshan-Sw <roshan.sam@spiderworks.in>
2025-07-18 13:19:23 +0530

    updates

--- src/employee-note/employee-note.service.ts
+++ src/employee-note/employee-note.service.ts
@@ -60,6 +60,24 @@ export class EmployeeNoteService {
         skip,
         take: limit,
         orderBy: { id: 'desc' },
+        include: {
+          createdBy: {
+            select: {
+              id: true,
+              first_name: true,
+              last_name: true,
+              email: true,
+            },
+          },
+          updatedBy: {
+            select: {
+              id: true,
+              first_name: true,
+              last_name: true,
+              email: true,
+            },
+          },
+        },
       }),
       this.prisma.employeeNote.count({ where }),
     ]);
